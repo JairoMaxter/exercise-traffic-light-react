65f53be229c86e736ba19d897f78ae0ac2adcbb4
JairoMaxter <jairomaxter@gmail.com>
2021-10-21 01:08:40 +0000

    solo el css y el basi app.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const App = () => {
+
+    return (
+        <>
+            <div className="body">
+                <div className="redligh">
+                </div>
+                <div className="yellowligh">
+                </div>
+                <div className="greenligh">
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App;
